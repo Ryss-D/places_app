eec69e18d87a99e81e275554a1ea494bc07ce4c0
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-07-27 19:30:48 -0500

    feat: adeed basic screens and providers

---
+++ lib/models/place.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+class PlaceLocation {
+  final double latitude;
+  final double longitud;
+  final String address;
+
+  PlaceLocation({
+    required this.latitude,
+    required this.longitud,
+    this.address = '',
+  });
+}
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.image,
+  });
+}
